b1a7b0927e16b93a8ad9fe387e34c49c220bf44b
n-cekic <neckic13@gmail.com>
2024-11-16 23:33:54 +0100

    add flags

--- cmd/parserd/parserd.go
+++ cmd/parserd/parserd.go
@@ -3,12 +3,20 @@ package main
 import (
 	"ethTx/cmd/tools/logging"
 	"ethTx/parser"
+	"flag"
+	"time"
+)
+
+var (
+	rpcURL        = flag.String("rpc.URL", "https://ethereum-rpc.publicnode.com", "Node rpc URL")
+	parseInterval = flag.Duration("parse.interval", time.Second, "Interval on which to query for new block")
+	logLevel      = flag.String("log.level", "info", "Logging level: `info` OR `debug`")
 )
 
 func main() {
-	logging.Init()
-	rpcURL := "https://ethereum-rpc.publicnode.com"
-	parser := parser.NewBlockParser(rpcURL)
+	logging.Init(*logLevel)
+	// rpcURL := "https://ethereum-rpc.publicnode.com"
+	parser := parser.NewBlockParser(*rpcURL, *parseInterval)
 
 	// Subscribe to addresses
 	parser.Subscribe("0x123")
